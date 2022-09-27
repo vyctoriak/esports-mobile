e9dba69d3ce24def704b95476a3fc15d0a17d4f9
vyctoria karina <vyctoriak@gmail.com>
2022-09-27 16:26:13 +0200

    screen splash, home interface, content list with liner gradient effect

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -2,25 +2,33 @@ import {
   ImageBackground,
   ImageSourcePropType,
   TouchableOpacity,
+  TouchableOpacityProps,
+  Text,
 } from 'react-native';
-
+import { LinearGradient } from 'expo-linear-gradient';
+import { THEME } from '../../theme';
 import { styles } from './styles';
 
 export interface GameCardProps {
   id: string;
-  nome: string;
+  name: string;
   ads: string;
   cover: ImageSourcePropType;
 }
 
-interface Props {
+interface Props extends TouchableOpacityProps {
   data: GameCardProps;
 }
 
-export function GameCard({ data }: Props) {
+export function GameCard({ data, ...rest }: Props) {
   return (
-    <TouchableOpacity style={styles.container}>
-      <ImageBackground source={data.cover} style={styles.cover} />
+    <TouchableOpacity style={styles.container} {...rest}>
+      <ImageBackground source={data.cover} style={styles.cover}>
+        <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
+          <Text style={styles.name}>{data.name}</Text>
+          <Text style={styles.ads}>{data.ads} anúncios</Text>
+        </LinearGradient>
+      </ImageBackground>
     </TouchableOpacity>
   );
 }
