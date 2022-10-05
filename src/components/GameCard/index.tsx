69effc3dc9eedc541788f558b1e53e9168073790
vyctoria karina <vyctoriak@gmail.com>
2022-10-05 15:54:16 +0200

    connecting with backend and listing all games

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,6 +1,5 @@
 import {
   ImageBackground,
-  ImageSourcePropType,
   TouchableOpacity,
   TouchableOpacityProps,
   Text,
@@ -11,9 +10,11 @@ import { styles } from './styles';
 
 export interface GameCardProps {
   id: string;
-  name: string;
-  ads: string;
-  cover: ImageSourcePropType;
+  title: string;
+  _count: {
+    ads: number;
+  };
+  bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -21,12 +22,21 @@ interface Props extends TouchableOpacityProps {
 }
 
 export function GameCard({ data, ...rest }: Props) {
+  function PluralOrSingularAds() {
+    if (data._count.ads > 1) {
+      return `${data._count.ads} anúncios`;
+    } else if (data._count.ads === 0) {
+      return 'Nenhum anúncio';
+    } else {
+      return `${data._count.ads} anúncio`;
+    }
+  }
   return (
     <TouchableOpacity style={styles.container} {...rest}>
-      <ImageBackground source={data.cover} style={styles.cover}>
+      <ImageBackground source={{ uri: data.bannerUrl }} style={styles.cover}>
         <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
-          <Text style={styles.name}>{data.name}</Text>
-          <Text style={styles.ads}>{data.ads} anúncios</Text>
+          <Text style={styles.name}>{data.title}</Text>
+          <Text style={styles.ads}>{PluralOrSingularAds()}</Text>
         </LinearGradient>
       </ImageBackground>
     </TouchableOpacity>
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,19 @@
+import { useEffect, useState } from 'react';
 import { View, Image, FlatList } from 'react-native';
 import { styles } from './styles';
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { Heading } from '../../components/Header/index';
-import { GameCard } from '../../components/GameCard';
-import { GAMES } from '../../utils/games';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() => {
+    fetch('http://192.168.178.27:3333/games')
+      .then((response) => response.json())
+      .then((data) => setGames(data));
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image source={logoImg} style={styles.logo} />
@@ -16,7 +24,7 @@ export function Home() {
 
       <FlatList
         contentContainerStyle={styles.contentList}
-        data={GAMES}
+        data={games}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <GameCard data={item} />}
         horizontal
