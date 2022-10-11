a5fb289a6598a99c3f3b6875c08e320c2d5a84b3
vyctoria karina <vyctoriak@gmail.com>
2022-10-11 13:16:09 +0200

    connecting to backend

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,6 +1,6 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useRoute, useNavigation } from '@react-navigation/native';
-import { TouchableOpacity, View, Image } from 'react-native';
+import { TouchableOpacity, View, Image, FlatList, Text } from 'react-native';
 import { GameParams } from '../../@types/navigation';
 import { Entypo } from '@expo/vector-icons';
 import { THEME } from '../../theme';
@@ -8,15 +8,26 @@ import { Background } from '../../components/Background';
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { styles } from './styles';
 import { Heading } from '../../components/Header';
+import { DuoCard, DuoCardProps } from '../../components/DuoCard';
+import { useEffect, useState } from 'react';
 
 export function Game() {
   const navigation = useNavigation();
   const route = useRoute();
   const game = route.params as GameParams;
+  const [duos, setDuos] = useState<DuoCardProps[]>();
 
   function handleGoBack() {
     navigation.goBack();
   }
+
+  useEffect(() => {
+    fetch(`http://192.168.178.27:3333/games/${game.id}/ads`)
+      .then((response) => response.json())
+      .then((data) => {
+        setDuos(data);
+      });
+  }, []);
 
   return (
     <Background>
@@ -33,7 +44,31 @@ export function Game() {
           <Image source={logoImg} style={styles.logo} />
           <View style={styles.right} />
         </View>
+
+        <Image
+          source={{ uri: game.bannerUrl }}
+          style={styles.cover}
+          resizeMode="cover"
+        />
+
         <Heading title={game.title} subtitle="Conecte-se e comece a jogar" />
+
+        <FlatList
+          data={duos}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <DuoCard data={item} onConect={() => {}} />}
+          horizontal
+          style={styles.containerList}
+          contentContainerStyle={[
+            duos.length > 0 ? styles.contentList : styles.emptyListContent,
+          ]}
+          showsHorizontalScrollIndicator={false}
+          ListEmptyComponent={() => (
+            <Text style={styles.emptyListText}>
+              Não há anúncios publicados para esse jogo ainda.
+            </Text>
+          )}
+        />
       </SafeAreaView>
     </Background>
   );
